b5bcdc1f1af0e4c7cff3de4e5a4ee7495b8176bf
gichuhiii <gnwcocoe@gmail.com>
2023-02-17 11:55:19 +0300

    Savings details .kt

--- app/src/main/java/com/example/mysavings/SavingsDetails.kt
+++ app/src/main/java/com/example/mysavings/SavingsDetails.kt
@@ -1,4 +1,25 @@
 package com.example.mysavings
 
-class SavingsDetails {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SavingsDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_savingsdetails)
+
+        val savebtn = findViewById<Button>(R.id.savebtn)
+        savebtn.setOnClickListener{
+            val Intent = Intent(this,SavingsHome::class.java)
+            startActivity(Intent)
+        }
+
+        val cancelbutton = findViewById<Button>(R.id.cancelbutton)
+        cancelbutton.setOnClickListener{
+            val Intent = Intent(this,Home::class.java)
+            startActivity(Intent)
+        }
+    }
 }
