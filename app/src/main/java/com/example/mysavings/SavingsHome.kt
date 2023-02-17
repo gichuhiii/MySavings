ec295d8c574948c3e7fdc24b3a9efdca172580d2
gichuhiii <gnwcocoe@gmail.com>
2023-02-17 11:27:27 +0300

    savings home .kt

--- app/src/main/java/com/example/mysavings/SavingsHome.kt
+++ app/src/main/java/com/example/mysavings/SavingsHome.kt
@@ -1,4 +1,43 @@
 package com.example.mysavings
 
-class SavingsHome {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SavingsHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_savingshome)
+
+        val newsavingsbtn = findViewById<Button>(R.id.newsavingsbtn)
+        newsavingsbtn.setOnClickListener{
+            val Intent = Intent(this,SavingsHome::class.java)
+            startActivity(Intent)
+        }
+
+        val emergencysavingsbtn = findViewById<Button>(R.id.emergencysavingsbtn)
+        emergencysavingsbtn.setOnClickListener{
+            val Intent = Intent(this,SavingsHome::class.java)
+            startActivity(Intent)
+        }
+
+        val bdaysavingsbtn = findViewById<Button>(R.id.bdaysavingsbtn)
+        bdaysavingsbtn.setOnClickListener{
+            val Intent = Intent(this,SavingsHome::class.java)
+            startActivity(Intent)
+        }
+
+        val addsavingsbtn = findViewById<Button>(R.id.addsavingsbtn)
+        addsavingsbtn.setOnClickListener{
+            val Intent = Intent(this,Home::class.java)
+            startActivity(Intent)
+        }
+
+        val cancelbutton = findViewById<Button>(R.id.cancelbutton)
+        cancelbutton.setOnClickListener{
+            val Intent = Intent(this,WelcomePage::class.java)
+            startActivity(Intent)
+        }
+    }
 }
